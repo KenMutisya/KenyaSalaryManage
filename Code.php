67978e7b6aac5f6a68e9f658b02581a6221ef363
Kennedy Mutisya <kenmsh@gmail.com>
2017-11-05 00:43:19 +0300

    Update Code.php

--- Code.php
+++ Code.php
@@ -3,21 +3,45 @@
  * Created by PhpStorm.
  * User: Kennedy Mutisya
  * Date: 04/11/2017
- * Time: 22:23
+ * Time: 20:59
  */
 
-namespace App\Radi\Mpesa\Searches;
+namespace App\Radi\Mpesa;
 
 
-class Code
+use App\Radi\Mpesa\Searches\Amount;
+use App\Radi\Mpesa\Searches\Code;
+use App\Radi\Mpesa\Searches\Date;
+use App\Radi\Mpesa\Searches\Name;
+use App\Radi\Mpesa\Searches\Phonenumber;
+use App\Radi\Mpesa\Searches\Time;
+
+class Mpesa
 {
+    protected $inspections = [
+        Code::class,
+        Amount::class,
+        Name::class,
+        Phonenumber::class,
+        Time::class,
+        Date::class,
 
-    public function detect($message){
-        $pattern = "/([[:alnum:]]{9})[[:blank:]](Confirmed)/is";
-        preg_match($pattern, $message, $matches, PREG_OFFSET_CAPTURE, 0);
-        if (isset($matches[1]) && !empty($matches)) {
-            return ['Transaction Code' => $matches[1][0]];
+    ];
+
+    public function detect($message)
+    {
+        $parts = [];
+
+        foreach ($this->inspections as $inspection) {
+
+            foreach ((new $inspection)->detect($message) as $key => $value) {
+
+                $parts[$key] = $value;
+            }
         }
-        return null;
+
+        return $parts;
     }
+
+
 }
